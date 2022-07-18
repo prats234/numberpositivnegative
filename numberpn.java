97b49c9b882ae2de7208e17465c7324925f4c6c0
prats234 <109065276+prats234@users.noreply.github.com>
2022-07-18 20:31:53 +0530

    Add files via upload

---
+++ numberpn.java
@@ -0,0 +1,16 @@
+public class numberpn
+{
+    public static void main (String[] args) {
+        double number = 0;
+        if (number > 0) {
+            System.out.println("Number is positive");
+        } else if (number < 0)
+        {
+            System.out.println("Number is negative");
+        }
+        else
+        {
+            System.out.println("Number is Zero");
+        }
+    }
+}
